5d39004905fa2005c2c9bc22695eb9416882b75c
savvysiddharth <savvysiddharth@gmail.com>
2019-05-28 15:09:57 +0530

    Updates

---
+++ precache-manifest.13fe1d9e3df827943f156ba7719af64f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "249229bbd41c298f7bf9b400809f65cb",
+    "url": "/flix-search-reactjs/index.html"
+  },
+  {
+    "revision": "8fc0d685f22f4d711d11",
+    "url": "/flix-search-reactjs/static/css/main.2b999b0c.chunk.css"
+  },
+  {
+    "revision": "e5322ac821f12dcb64ce",
+    "url": "/flix-search-reactjs/static/js/2.ddd9a8c0.chunk.js"
+  },
+  {
+    "revision": "8fc0d685f22f4d711d11",
+    "url": "/flix-search-reactjs/static/js/main.bb8454a9.chunk.js"
+  },
+  {
+    "revision": "d7978caa19714d539cc3",
+    "url": "/flix-search-reactjs/static/js/runtime~main.5b212955.js"
+  },
+  {
+    "revision": "afdb8fabd2d16878562539eea882bd93",
+    "url": "/flix-search-reactjs/static/media/loading.afdb8fab.svg"
+  },
+  {
+    "revision": "7dbe1adca0843a48fa9e4152c88af80b",
+    "url": "/flix-search-reactjs/static/media/logo.7dbe1adc.svg"
+  }
+]);
